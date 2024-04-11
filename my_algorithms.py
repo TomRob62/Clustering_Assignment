f8dfb541ef4039c330e5122e8e293c54bc34da91
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-04-11 15:39:43 -0400

    Creating algorithms.py

---
+++ my_algorithms.py
@@ -0,0 +1,21 @@
+"""
+Thomas Roberts
+CS 4267(01) Machine Learning
+Professor Ojo
+April 11, 2024
+
+Program Desciption: This program contains all the algorithms that will be
+used in the main program to classify several datasets
+"""
+
+class My_Algorithms:
+    """
+    Desciptions:
+
+    Attributes
+    ----------
+
+    Functions
+    ---------
+    """
+    
