1b789fea6e8b2513e1111235c038b798cae85f8d
Thomas Roberts <139529054+TomRob62@users.noreply.github.com>
2024-04-12 23:40:30 -0400

    Creating datset_manager program
    This program is intended to manage dataset creation and splitting.

---
+++ dataset_manager.py
@@ -0,0 +1,13 @@
+"""
+Thomas Roberts
+CS 4267(01) Machine Learning
+Professor Ojo
+April 11, 2024
+
+Program Desciption: A dataset manager to handle dataset creation and
+handling train/test splits.
+"""
+class Dataset_Manager:
+    """
+    
+    """
